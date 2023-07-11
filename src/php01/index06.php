dd592b62f8d0097a53dd02ac0c9ffd4fa1fa4272
ichinari akiho <gootaen@gmail.com>
2023-07-11 21:47:50 +0900

    サンプルファイルの追加

---
+++ src/php01/index06.php
@@ -0,0 +1,13 @@
+<?php
+function exam($score1, $score2, $score3)
+{
+    $total = $score1 + $score2 + $score3;
+    if($total > 210){
+        echo "合計点は".$total."点なので合格です。";
+    }
+    else{
+        echo "合計点は".$total."点なので不合格です。";
+    }
+}
+
+echo (exam(80,60,90));
